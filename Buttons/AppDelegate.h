b90b0d5bbe2278f6f1683818bebe0cba3d623dd6
BridgeLabz <bridgelabz@narayans-macbook-pro-2.local>
2015-12-21 12:42:16 +0530

    Initial Commit

---
+++ Buttons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Buttons
+//
+//  Created by BridgeLabz on 21/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Buttons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Buttons
+//
+//  Created by BridgeLabz on 21/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
